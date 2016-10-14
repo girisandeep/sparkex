f8a3154d6ba094a8443c5bec2c38e7b532b2cde4
Sandeep Giri <sandeepgiri@gmail.com>
2016-10-14 16:48:33 +0530

    Hello World with scala

---
+++ helloworld/SimpleApp.scala
@@ -0,0 +1,18 @@
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+
+object SimpleApp {
+  def main(args: Array[String]) {
+	val logFile = "/data/mr/wordcount/input" // Should be some file on your system
+	val conf = new SparkConf().setAppName("Simple Application")
+	val sc = new SparkContext(conf)
+	val logData = sc.textFile(logFile)
+	
+	val output = logData.take(10)
+	for(x <- output){
+		println(x);
+	}
+  }
+}
+
---
+++ helloworld/build.sbt
@@ -0,0 +1,7 @@
+name := "tasky"
+version := "0.1.0"
+
+libraryDependencies ++= Seq(
+"org.apache.spark" %% "spark-core" % "1.5.2" % "provided"
+)
+
